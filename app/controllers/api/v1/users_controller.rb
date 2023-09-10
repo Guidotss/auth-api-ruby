929a300ae51bfe0279b2ba1ec21444970eeba2d3
Guidotss <guidoolguin@hotmail.com>
2023-09-10 18:42:27 -0300

    Creando usuario en base de datos

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.all
+    if @users
+      render json: {ok: true, users: @users}, status: :ok
+    else
+      render json: { ok: false, message: 'No users found' }, status: :not_found
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { ok: true, user: @user }, status: :created
+    else
+      render json: { ok: false, message: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+  def user_params
+    JSON.parse(request.body.read).symbolize_keys
+  end
+end
